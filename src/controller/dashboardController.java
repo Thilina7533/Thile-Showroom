5514f02d202470877b3861e1355f30dd40daaa44
Thilina Dilshan <ourvillage.td@gmail.com>
2020-09-21 14:37:04 +0530

    "first commit"

--- src/controller/dashboardController.java
+++ src/controller/dashboardController.java
@@ -2,12 +2,7 @@ package controller;
 
 
 import animatefx.animation.*;
-
-import animatefx.util.ParallelAnimationFX;
-import animatefx.util.SequentialAnimationFX;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -17,54 +12,56 @@ public class dashboardController {
     public Pane context;
 
 
-
     private void setUi(String location) throws IOException {
         context.getChildren().clear();
         context.getChildren().add(FXMLLoader.load(this.getClass().
                 getResource("/view/" + location + ".fxml")));
     }
 
-    public void DashBoardOnAction(ActionEvent actionEvent) throws IOException {
+    public void DashBoardOnAction() throws IOException {
         setUi("DashboardForm");
         new Flash(context).play();
 
     }
 
-    public void AddItemOnAction(ActionEvent actionEvent) throws IOException {
+    public void AddItemOnAction() throws IOException {
         setUi("AddItemForm");
         new Flip(context).play();
 
     }
 
-    public void btnAddCashier(ActionEvent actionEvent) throws IOException {
+    public void btnAddCashier() throws IOException {
         setUi("AddCashierForm");
         new FadeInUp(context).play();
     }
 
 
-    public void btnLogOut(ActionEvent actionEvent) throws IOException {
+    public void btnLogOut() {
         Stage stage = (Stage) context.getScene().getWindow();
         stage.close();
     }
 
-    public void btnAddCustomer(ActionEvent actionEvent) throws IOException {
+    public void btnAddCustomer() throws IOException {
         setUi("AddCustomerForm");
         new BounceIn(context).play();
     }
-    public void btnReportOnAction(ActionEvent actionEvent) throws IOException {
+
+    public void btnReportOnAction() throws IOException {
         setUi("Report");
         new ZoomIn(context).play();
     }
-    public void btnAboutOnAction(ActionEvent actionEvent) throws IOException {
+
+    public void btnAboutOnAction() throws IOException {
         setUi("AboutForm");
         new RotateIn(context).play();
     }
-    public void btnSuplayerOnAction(ActionEvent actionEvent) throws IOException {
+
+    public void btnSuplayerOnAction() throws IOException {
         setUi("AddSuply");
         new RubberBand(context).play();
     }
 
-    public void btnCloaseOnAction(ActionEvent actionEvent) {
+    public void btnCloaseOnAction() {
         Stage stage = (Stage) context.getScene().getWindow();
         stage.close();
     }
